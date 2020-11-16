f2b0722a51f291184812b47ab8b1fbea9cea229b
Niko-PC\Niko <nicodude00@gmail.com>
2020-11-16 19:28:01 +0200

    juni

--- PongPeli/FakeAI.ino
+++ PongPeli/FakeAI.ino
@@ -5,16 +5,16 @@ if(paddle_update && currentMode==1) {
 
 const uint8_t half_paddle = playerSize >> 1;
 
-if(playerPosY2 + half_paddle > ballY){  
+if(playerPosY2 + half_paddle > ballY){  //Tarkistaa että onko tietokoneen mailan sijainti ylempänä mistä pallo on tulossa, jos on niin liikuttaa mailaa alaspäin.
     MovePlayerDown(0);
 }
 
-if(playerPosY2 + half_paddle < ballY) {
+if(playerPosY2 + half_paddle < ballY) { //Tarkistaa että onko mailan sijainti alempana kuin mistä pallo on tulossa ja jos on liikuttaa mailaa ylöspäin.
     MovePlayerUp(0);
 }
 
 if(playerPosY2 < 1) playerPosY2 = 1;
 
-if(playerPosY2 + playerSize > 128) playerPosY2 = 128 - playerSize;
+if(playerPosY2 + playerSize > 128) playerPosY2 = 128 - playerSize; //128 arvoa säätelemällä voidaan määrittää kuinka alhaalla tai ylhäällä tietokoneen maila käy = Periaatteessa kuinka helposti se osuu siihen.
 }
 }
